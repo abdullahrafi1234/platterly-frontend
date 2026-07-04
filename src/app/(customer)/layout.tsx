9a1d1f97fce6ff89b3fd0a29b39768b871e564cd
abdullahrafi1234 <rafibhuiyan1234@gmail.com>
2026-07-04 21:51:16 +0600

    create custom loader

---
+++ src/app/(customer)/layout.tsx
@@ -0,0 +1,16 @@
+"use client";
+import Loader from "@/components/shared/Loader";
+
+import { useRequireRole } from "@/hooks/useRequireRole";
+
+export default function CustomerLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { loading } = useRequireRole("CUSTOMER");
+
+  if (loading) return <Loader></Loader>;
+
+  return <>{children}</>;
+}
---
+++ src/components/shared/Loader.tsx
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex items-center justify-center py-20">
+      <div className="w-8 h-8 border-4 border-ink/10 border-t-chili rounded-full animate-spin" />
+    </div>
+  );
+}
